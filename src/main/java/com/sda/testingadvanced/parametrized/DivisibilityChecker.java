f8d9a185bdea183e4d99a859d3e4de13cb15ac0f
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-05 16:34:03 +0100

    -- initial commit

---
+++ src/main/java/com/sda/testingadvanced/parametrized/DivisibilityChecker.java
@@ -0,0 +1,9 @@
+package com.sda.testingadvanced.parametrized;
+
+public class DivisibilityChecker {
+
+	public static boolean isDivisibleBy3(int number) {
+		return false;
+	}
+
+}
---
+++ src/main/java/com/sda/testingadvanced/parametrized/PalindromeChecker.java
@@ -0,0 +1,8 @@
+package com.sda.testingadvanced.parametrized;
+
+public class PalindromeChecker {
+
+	public static boolean isPalindrome(String text) {
+		return false;
+	}
+}
