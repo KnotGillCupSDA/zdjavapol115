affb3c814205c22c8e3f0bd716b6475c940394f4
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-06 10:48:48 +0100

    PalindromeChecker NullSource

--- src/main/java/com/sda/testingadvanced/parametrized/DivisibilityChecker.java
+++ src/main/java/com/sda/testingadvanced/parametrized/DivisibilityChecker.java
@@ -2,7 +2,10 @@ package com.sda.testingadvanced.parametrized;
 
 public class DivisibilityChecker {
 
-	public static boolean isDivisibleBy3(int number) {
+	public static boolean isDivisibleBy3(Integer number) {
+		if(number == null) {
+			return false;
+		}
 		return number % 3 == 0;
 	}
 
--- src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
@@ -6,19 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class DivisibilityCheckerTest {
 
 	@ParameterizedTest
 	@ValueSource(ints = { -9, 0, 9, 27 })
-	void shouldBeDivisibleBy3(int number) {
+	void shouldBeDivisibleBy3(Integer number) {
 		assertTrue(DivisibilityChecker.isDivisibleBy3(number));
 	}
 
 	@ParameterizedTest
+	@NullSource
 	@ValueSource(ints = { 2, -8, 4, 61 })
-	void shouldNotBeDivisibleBy3(int number) {
+	void shouldNotBeDivisibleBy3(Integer number) {
 		//when
 		boolean notDivisibleBy3 = DivisibilityChecker.isDivisibleBy3(number);
 
