c3f17c16e9ba496478201234d513e6cb73d12692
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-06 09:27:58 +0100

    DivisibilityChecker implemented

--- src/main/java/com/sda/testingadvanced/parametrized/DivisibilityChecker.java
+++ src/main/java/com/sda/testingadvanced/parametrized/DivisibilityChecker.java
@@ -3,7 +3,7 @@ package com.sda.testingadvanced.parametrized;
 public class DivisibilityChecker {
 
 	public static boolean isDivisibleBy3(int number) {
-		return false;
+		return number % 3 == 0;
 	}
 
 }
