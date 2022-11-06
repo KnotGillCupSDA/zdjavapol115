44b81f027a1f0f8153a538590cc96c4ffb6e3ddf
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-06 09:54:19 +0100

    DivisibilityCheckerTest negative test with @ValueSource

--- src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
@@ -1,5 +1,6 @@
 package com.sda.testingadvanced.parametrized;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,5 +12,15 @@ class DivisibilityCheckerTest {
 	@ValueSource(ints = {-9, 0, 9, 27})
 	void shouldBeDivisibleBy3(int number) {
 		assertTrue(DivisibilityChecker.isDivisibleBy3(number));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {2, -8, 4, 61})
+	void shouldNotBeDivisibleBy3(int number) {
+		//when
+		boolean notDivisibleBy3 = DivisibilityChecker.isDivisibleBy3(number);
+
+		//then
+		assertFalse(notDivisibleBy3);
 	}
 }
