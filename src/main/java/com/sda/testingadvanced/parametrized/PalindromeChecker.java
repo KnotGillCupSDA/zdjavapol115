299a032341809c45d96d96627494825e1c47c7dd
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-06 10:14:10 +0100

    PalindromeChecker implemented

--- src/main/java/com/sda/testingadvanced/parametrized/PalindromeChecker.java
+++ src/main/java/com/sda/testingadvanced/parametrized/PalindromeChecker.java
@@ -2,7 +2,36 @@ package com.sda.testingadvanced.parametrized;
 
 public class PalindromeChecker {
 
+	/**
+	 * @return true if text is a palindrome
+	 */
 	public static boolean isPalindrome(String text) {
-		return false;
+		// abba, kajak
+		int i = 0;
+		int j = text.length() - 1;
+
+		while (i < text.length() && j >= 0 && i <= j) {
+			if (text.charAt(i) != text.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+
+		return true;
+	}
+
+	public static boolean isPalindromeIwo(String text) {
+		String reversedString = "";
+		char ch;
+		for (int i = 0; i < text.length(); i++) {
+			ch = text.charAt(i);
+			reversedString = ch + reversedString;
+		}
+		return reversedString.equals(text);
+	}
+
+	public static boolean isPalindromeStringBuilder(String text) {
+		return new StringBuilder(text).reverse().toString().equals(text);
 	}
 }
