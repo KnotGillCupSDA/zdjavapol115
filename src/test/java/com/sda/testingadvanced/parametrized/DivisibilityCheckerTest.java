23faab1f76763c3ba7f0cfcfd4068d89c4968a0e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-06 10:01:09 +0100

    DivisibilityCheckerTest negative test with @CsvSource

--- src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
@@ -1,21 +1,23 @@
 package com.sda.testingadvanced.parametrized;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class DivisibilityCheckerTest {
 
 	@ParameterizedTest
-	@ValueSource(ints = {-9, 0, 9, 27})
+	@ValueSource(ints = { -9, 0, 9, 27 })
 	void shouldBeDivisibleBy3(int number) {
 		assertTrue(DivisibilityChecker.isDivisibleBy3(number));
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = {2, -8, 4, 61})
+	@ValueSource(ints = { 2, -8, 4, 61 })
 	void shouldNotBeDivisibleBy3(int number) {
 		//when
 		boolean notDivisibleBy3 = DivisibilityChecker.isDivisibleBy3(number);
@@ -23,4 +25,11 @@ class DivisibilityCheckerTest {
 		//then
 		assertFalse(notDivisibleBy3);
 	}
+
+	@ParameterizedTest
+	@CsvSource(value = { "-1, false", "0, true", "1, false", "3, true", "9, true" })
+	void testDivisibilityBy3(int number, boolean expected) {
+		assertEquals(expected, DivisibilityChecker.isDivisibleBy3(number));
+	}
+
 }
