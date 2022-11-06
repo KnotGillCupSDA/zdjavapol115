b01ca21f7d1ef27b7cdd8d3aff0107d9e408b6a0
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-06 15:48:49 +0100

    DatabaseStoreTest tested

--- src/test/java/com/sda/testingadvanced/mockito/databasestore/DatabaseStoreTest.java
+++ src/test/java/com/sda/testingadvanced/mockito/databasestore/DatabaseStoreTest.java
@@ -1,6 +1,7 @@
 package com.sda.testingadvanced.mockito.databasestore;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -30,5 +31,33 @@ class DatabaseStoreTest {
 
 		verify(databaseConnection, times(1)).isOpened();
 		verify(databaseConnection, never()).open();
+	}
+
+	@Test
+	void shouldOpenDatabaseConnectionAndAddData() {
+
+		when(databaseConnection.isOpened())
+				.thenReturn(false)
+				.thenReturn(true);
+
+		//Sprawdź, czy operacja addData się powiodła
+		assertThat(databaseStore.addData(DATA))
+				.isEqualTo(1);
+
+		//Sprawdź czy zawołano predykat isOpened() dwukrotnie
+		verify(databaseConnection, times(2)).isOpened();
+
+		//Zweyfikuj, że zawołano metodę open()
+		verify(databaseConnection, times(1)).open();
+	}
+
+	@Test
+	void shouldThrowExceptionWhenCantOpenDatabaseConnection() {
+		//za każdym razem jak wolany jest predykat isOpened to zwróc false
+		when(databaseConnection.isOpened()).thenReturn(false);
+
+		//sprawdź, że dodanie czegokolwiek spowoduje rzucenie wyjątku o typie SdaException
+		assertThatThrownBy(()-> databaseStore.addData(DATA))
+				.isInstanceOf(SdaException.class);
 	}
 }
