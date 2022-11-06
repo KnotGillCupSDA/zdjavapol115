e8a2d9a96cc93747e271678f143ecc50621daeda
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-06 11:31:29 +0100

    RomanNumeralConverterTest from Rafal and Bartek

--- src/test/java/com/sda/testingadvanced/parametrized/romannumeral/RomanNumeralConverterTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/romannumeral/RomanNumeralConverterTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 
 class RomanNumeralConverterTest {
@@ -18,5 +19,18 @@ class RomanNumeralConverterTest {
 	@EnumSource
 	void shouldAlwaysBeAPositiveNumber(RomanNumeralConverter.ArabicToRoman arabicToRoman) {
 		assertThat(arabicToRoman.getArabic()).isPositive();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = { "CLXXXIX, 189", "MMXXII, 2022" })
+	void shouldConvertNumbersRafal(String roman, int arabic) {
+		assertThat(RomanNumeralConverter.romanFor(arabic)).isEqualTo(roman);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = { "321, CCCXXI", "111, CXI" })
+	void shouldBeTheSameBeforeConvertBartek(int number, String expected) {
+		assertEquals(expected, RomanNumeralConverter.romanFor(number));
+
 	}
 }
