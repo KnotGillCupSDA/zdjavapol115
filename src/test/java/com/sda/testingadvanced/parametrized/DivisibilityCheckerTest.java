1bb61085a3d98bb3d06d0c4ae549868880fa26e0
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-11-06 09:42:29 +0100

    DivisibilityCheckerTest more test cases

--- src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
@@ -20,9 +20,45 @@ class DivisibilityCheckerTest {
 	}
 
 	@Test
+	void negativeNumberShouldBeDivisibleBy3() {
+		//given
+		int number = -9;
+
+		//when
+		boolean divisibleBy3 = DivisibilityChecker.isDivisibleBy3(number);
+
+		//then
+		assertTrue(divisibleBy3);
+	}
+
+	@Test
+	void zeroShouldBeDivisibleBy3() {
+		//given
+		int number = 0;
+
+		//when
+		boolean divisibleBy3 = DivisibilityChecker.isDivisibleBy3(number);
+
+		//then
+		assertTrue(divisibleBy3);
+	}
+
+	@Test
 	void shouldNotBeDivisibleBy3() {
 		//given
 		int number = 10;
+
+		//when
+		boolean divisibleBy3 = DivisibilityChecker.isDivisibleBy3(number);
+
+		//then
+		assertFalse(divisibleBy3);
+	}
+
+	@Test
+	void negativeNumberShouldNotBeDivisibleBy3() {
+		//given
+		int number = -10;
 
 		//when
 		boolean divisibleBy3 = DivisibilityChecker.isDivisibleBy3(number);
